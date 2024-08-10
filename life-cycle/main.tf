945a94c1bafc8646e6a85b9ba79f5e566c6f8ce7
Gangadhar <gangadharamchalla66@gmail.com>
2024-08-11 04:06:08 +0530

    this is updated one

---
+++ life-cycle/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  
+}
+
+
+resource "aws_instance" "dev" {
+  ami = "ami-0a4408457f9a03be3"
+  instance_type = "t2.micro"
+  key_name = "God"
+
+  tags = {
+    name = "today"
+  }
+
+
+
+lifecycle {
+    prevent_destroy=true
+
+}
+
+}
